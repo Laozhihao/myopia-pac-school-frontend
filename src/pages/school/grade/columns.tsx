c5e19f286f93056f9c20ebc8ec2c6ebb4d4e5818
Tree <lshf1437@163.com>
2022-03-10 15:05:09 +0800

    feat: 年级班级新增保存按钮

--- src/pages/school/grade/columns.tsx
+++ src/pages/school/grade/columns.tsx
@@ -1,15 +1,14 @@
 import type { ProColumns } from '@ant-design/pro-table';
-import { Input, message } from 'antd';
+import { Input, message, Button } from 'antd';
 import { addClass, editClass } from '@/api/school';
 
-const onAddClass = async (e: any, record: any) => {
-  const { value } = e.target;
+const onAddClass = async (value: any, record: any) => {
   const isEdit = typeof record.id === 'number'; // 编辑/新增
-  const parm = { ...record, name: value };
+  const parm = { ...record };
   const apiFn = isEdit ? editClass : addClass;
   !isEdit && delete parm.id;
   const { data } = await apiFn(parm);
-  Object.assign(record, { id: isEdit ? record.id : data, name: value });
+  Object.assign(record, { id: isEdit ? record.id : data });
   message.success(isEdit ? '编辑成功' : '新建成功');
 };
 
@@ -22,13 +21,18 @@ export const listColumns: ProColumns<API.GradeListItem>[] = [
       return !record.gradeId ? (
         <span>{text}</span>
       ) : (
-        <Input
-          defaultValue={record.name}
-          style={{ width: 250 }}
-          onChange={(e) => Object.assign(record, { name: e.target.value })}
-          onPressEnter={(e) => onAddClass(e, record)}
-          placeholder="请输入班级名称，按回车确认"
-        />
+        <>
+          <Input
+            defaultValue={record.name}
+            style={{ width: 250 }}
+            onChange={(e) => Object.assign(record, { name: e.target.value })}
+            onPressEnter={(e) => onAddClass((e.target as HTMLInputElement).value, record)}
+            placeholder="请输入班级名称，按回车确认"
+          />
+          <Button type="link" onClick={() => onAddClass(record.name, record)}>
+            保存
+          </Button>
+        </>
       );
     },
   },

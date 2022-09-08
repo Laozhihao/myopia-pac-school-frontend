c7881dd867b9cec28a7776d8bb55de3fb2416aa8
liwenxin <wenxin.li@vistel.cn>
2022-09-08 18:34:16 +0800

    fix:学生数据表模板导入问题

--- config/config.dev.ts
+++ config/config.dev.ts
@@ -2,7 +2,7 @@
 import { defineConfig } from 'umi';
 
 export default defineConfig({
-  // publicPath: '/',
+  publicPath: '/',
   plugins: [
     // https://github.com/zthxxx/react-dev-inspector
     'react-dev-inspector/plugins/umi/react-inspector',

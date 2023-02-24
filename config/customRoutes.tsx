baadc3c0677c7a633c3e46efc9173100b4519b5d
liwenxin <wenxin.li@vistel.cn>
2023-02-24 14:36:11 +0800

    feat: 优化加载路由信息

--- config/customRoutes.tsx
+++ config/customRoutes.tsx
@@ -5,89 +5,21 @@ import {
   CommentOutlined,
   SettingOutlined,
 } from '@ant-design/icons';
+import routes from './routes';
 
-export default [
-  {
-    path: '/user',
-    layout: false,
-    routes: [
-      { path: '/user', routes: [{ name: '登录', path: '/user/login', component: './user/Login' }] },
-      { component: './404' },
-    ],
-  },
-  {
-    name: '学生管理',
-    icon: <ProfileOutlined />,
-    path: '/student',
-    hideChildrenInMenu: true,
-    routes: [
-      { path: '/student', component: './student/list' },
-      { name: '档案管理', path: '/student/file', component: './student/file' },
-    ],
-  },
-  {
-    name: '视力筛查',
-    icon: <EyeOutlined />,
-    path: '/screening',
-    routes: [
-      {
-        path: '/screening/notice',
-        component: './screening/notice',
-        name: '筛查通知',
-      },
-      {
-        path: '/screening/play',
-        component: './screening/play',
-        name: '筛查计划',
-      },
-      {
-        name: '筛查学生列表',
-        path: '/screening/play/student',
-        component: './screening/play/student',
-        hideInMenu: true,
-      },
-      {
-        path: '/screening/play/result',
-        component: './screening/result',
-        name: '筛查结果',
-        hideInMenu: true,
-      },
-    ],
-  },
-  {
-    name: '防控中心',
-    icon: <ProjectOutlined />,
-    path: '/prevention',
-    filter: 'isIndependentScreening',
-    routes: [
-      {
-        name: '眼健康中心',
-        path: '/prevention/eye-health',
-        component: './prevention/eye-health',
-      },
-      {
-        name: '视力小队',
-        path: '/prevention/vision',
-        component: './prevention/vision',
-      },
-      {
-        name: '数据报送',
-        path: '/prevention/data-receive',
-        component: './prevention/data-receive',
-      },
-    ],
-  },
-  { name: '消息中心', icon: <CommentOutlined />, path: '/info-center', component: './infoCenter' },
-  {
-    name: '学校管理',
-    icon: <SettingOutlined />,
-    path: '/school',
-    hideChildrenInMenu: true,
-    routes: [
-      { path: '/school', component: './school/manage' },
-      { name: '年级管理', path: '/school/grade', component: './school/grade' },
-    ],
-  },
-  { path: '/', redirect: '/student' },
-  { component: './404' },
-];
+// 存储 icon 信息替换 icon 组件
+const iconInfoMap = {
+  profile: <ProfileOutlined />,
+  eye: <EyeOutlined />,
+  project: <ProjectOutlined />,
+  comment: <CommentOutlined />,
+  setting: <SettingOutlined />,
+};
+
+export default routes.map((item) => {
+  if (item?.icon) {
+    // 直接用 routes 在 config 加载 jsx icon 组件会有时序问题
+    item.icon = iconInfoMap[item.icon];
+  }
+  return item;
+});

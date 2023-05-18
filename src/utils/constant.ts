60777c4e1bdac9e2d0952ed1e54d557932d3f8c6
liwenxin <wenxin.li@vistel.cn>
2023-05-18 15:20:51 +0800

    feat: 数据上报新增2022年

--- src/utils/constant.ts
+++ src/utils/constant.ts
@@ -234,6 +234,7 @@ export const ASTIGMATISM_LEVEL = {
 
 // 新疆系统对接特殊流程 - 年份
 export const XINJIANG_YEAR_OPTIONS = [
+  { value: 2022, label: 2022 },
   { value: 2023, label: 2023 },
   { value: 2024, label: 2024 },
   { value: 2025, label: 2025 },
